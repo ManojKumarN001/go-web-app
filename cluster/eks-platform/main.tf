9ba53adb69e95e70940d798cd865e6409a3eba0a
MANOJKUMAR N <107676423+ManojKumarN001@users.noreply.github.com>
2024-08-25 13:40:04 +0530

    code fixing

--- cluster/eks-platform/main.tf
+++ cluster/eks-platform/main.tf
@@ -5,31 +5,6 @@ provider "aws" {
   region = "us-east-1"
 } 
 
-
-#------------------------ terraform provider --------------#
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-
-#------------------- Terrform cloud backend -------------------#
-
-
-terraform {
-  cloud {
-    organization = "Terraform_Infra_Automation"
-    workspaces {
-      name = "Kubernetes-Cluster-workspace"
-    }
-  }
-}
-
 #----------------------- data sources ----------------------#
 
 data "aws_availability_zones" "available" {}
---
+++ cluster/eks-platform/provider.tf
@@ -0,0 +1,23 @@
+#------------------------ terraform provider --------------#
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+#------------------- Terrform cloud backend -------------------#
+
+
+terraform {
+  cloud {
+    organization = "Terraform_Infra_Automation"
+    workspaces {
+      name = "Kubernetes-Cluster-workspace"
+    }
+  }
+}
