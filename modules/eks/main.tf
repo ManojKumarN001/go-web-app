7f6d57836a4e676578efdaeb77f0380aab8b0fcb
MANOJKUMAR N <107676423+ManojKumarN001@users.noreply.github.com>
2024-08-25 15:13:37 +0530

    code fixing

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -46,7 +46,30 @@ resource "aws_iam_role_policy_attachment" "eks_cluster" {
 
 resource "aws_iam_role" "eks_node_group" {
   name               = "${var.cluster_name}-node-group-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.eks_node_group_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+
+data "aws_iam_policy_document" "eks_node_group_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "eks_node_group" {
@@ -77,16 +100,6 @@ resource "aws_security_group" "control_plane" {
   tags = merge(var.tags, { "Name" = "${var.name_prefix}/ControlPlaneSecurityGroup" })
 }
 
-data "aws_iam_policy_document" "eks_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
 
 # Uncomment if needed
 # resource "aws_eks_addon" "coredns" {
