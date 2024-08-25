f5c66142810116dbee85399cd519d5710cb48c2e
MANOJKUMAR N <107676423+ManojKumarN001@users.noreply.github.com>
2024-08-25 12:49:36 +0530

    moduliing fixing

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -75,10 +75,10 @@ resource "aws_route_table" "private" {
 
 
 resource "aws_route" "private_nat_gateway" {
-  count = length([for rt in aws_route_table.example : rt.id if rt.route.0.cidr_block != "0.0.0.0/0"])
-  route_table_id         = aws_route_table.example[count.index].id
+  count = length([for rt in aws_route_table.private : rt.id if rt.route.0.cidr_block != "0.0.0.0/0"])
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.example.id
+  nat_gateway_id         = aws_nat_gateway.this.id
 }
 
 resource "aws_route_table_association" "private" {
